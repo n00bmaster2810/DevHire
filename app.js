085914bef64059435cfa9df152c5433263a9f67a
Satya Prakash Satyam <satty9509@gmail.com>
2020-10-02 11:41:09 +0530

    passport login

--- app.js
+++ app.js
@@ -1,29 +1,45 @@
 const express = require('express');
 const path = require('path');
 const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
 
 const indexRouter = require('./routes/index');
 const developerRouter = require("./routes/developerRouter");
 const companyRouter = require("./routes/companyRouter");
-
+const registerRouter = require("./routes/registerRouter");
+const loginRouter = require("./routes/loginRouter");
 const app = express();
 const port = 3000 || process.env.PORT;
-
+const passport = require("passport")
 //mongoose connection setup using online cloud database
-const uri = "mongodb+srv://satya:satya@cluster0.8csuv.mongodb.net/<dbname>?retryWrites=true&w=majority";
-mongoose.connect(uri, { useNewUrlParser: true , useUnifiedTopology: true}, () => {
-  console.log("Mongoose connected");
+const uri = "mongodb+srv://satya:satya@cluster0.8csuv.mongodb.net/SHIELD?retryWrites=true&w=majority";
+mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true, useFindAndModify: true });
+const connection = mongoose.connection;
+connection.once("open", () => {
+  console.log("mongo connected");
+}).catch((err) => {
+  console.log("mongo not connected")
 });
+
+//passport config
+const passportInit = require("./config/passport")
+passportInit(passport)
+app.use(passport.initialize());
+app.use(passport.session());
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.urlencoded());
+app.use(express.json());
 
 app.use('/', indexRouter);
 app.use("/", developerRouter);
 app.use("/", companyRouter);
+app.use("/", registerRouter);
+app.use("/", loginRouter);
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
---
+++ config/passport.js
@@ -0,0 +1,40 @@
+const LocalStrategy = require("passport-local").Strategy;
+const User = require("../schema/userSchema");
+const bcrypt = require("bcrypt");
+
+const init = (passport) => {
+	passport.use(new LocalStrategy({ usernameField: "email" }, async (email, password, done) => {
+		//Login
+		//check if email exists
+		const user = await User.findOne({ email: email });
+		if (!user) {
+			return done(null, false, { message: "No user with this email found" })
+		}
+		try {
+			//comparing the password
+			bcrypt.compare(password, user.password).then(
+				match => {
+					if (match) {
+						return done(null, user, { message: "Logged in success" });
+					}
+					else return done(null, false, { message: "Wrong Password" });
+				}
+			);
+		}
+		//error
+		catch (err) {
+			return done(null, false, { message: "Something went wrong" });
+		}
+	}));
+	passport.serializeUser((user, done) => {
+		done(null, user._id)
+	});
+	passport.deserializeUser((ud, done) => {
+		User.findById(id, (err, user) => {
+			done(err, user)
+		});
+	});
+
+};
+
+module.exports = init;
