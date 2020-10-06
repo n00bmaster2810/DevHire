20500a10d979363267b96c633ca17c920bbcb4e0
yash24septem <yash24septem@gmail.com>
2020-10-06 16:09:43 +0530

    added company.ejs

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const path = require('path');
+
 const mongoose = require("mongoose");
 const bcrypt = require("bcrypt");
 
--- schema/myPortfolioSchema.js
+++ schema/myPortfolioSchema.js
@@ -39,6 +39,7 @@ var myPortfolio = new mongoose.Schema(
 		{
 		  data :Buffer,
 		  contentType :String
+		  
 
 		}
 
