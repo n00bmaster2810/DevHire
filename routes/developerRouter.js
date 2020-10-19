0dc94802a867e234502bf449daab1f96cd427f92
Satya Prakash Satyam <satty9509@gmail.com>
2020-10-19 22:14:19 +0530

    dev profile router

--- routes/developerRouter.js
+++ routes/developerRouter.js
@@ -3,8 +3,12 @@ const developerRouter = express.Router();
 const developerGuest = require("../middleware/developerGuest");
 
 /* GET developers page. */
-developerRouter.get("/developers", developerGuest, function (req, res) {
-  res.render("developers");
-});
+developerRouter
+  .get("/developers", developerGuest, function (req, res) {
+    res.render("developers");
+  })
+  .get("/developers/profile", developerGuest, (req, res) => {
+    res.render("devProfile");
+  });
 
 module.exports = developerRouter;
