1f9ff3b64cbff7f4d3c40a9a54e763b808773e4e
Satya Prakash Satyam <satty9509@gmail.com>
2020-11-02 07:34:55 +0530

    satya

--- public/javascript/companyProfile.js
+++ public/javascript/companyProfile.js
@@ -1,10 +1,10 @@
-//let updateSubsBtn = document.querySelectorAll(".subs");
+let updateSubsBtn = document.querySelectorAll(".subs");
 
-//updateSubsBtn.forEach(async (btn) => {
-//  $(btn).click(() => {
-//    let company = JSON.parse(btn.dataset.comp);
-//    console.log(company);
-//    let path = "/developers/subscribe/" + company._id;
-//    window.location = path;
-//  });
-//});
+updateSubsBtn.forEach(async (btn) => {
+  $(btn).click(() => {
+    let company = JSON.parse(btn.dataset.comp);
+    console.log(company);
+    let path = "/developers/subscribe/" + company._id;
+    window.location = path;
+  });
+});
